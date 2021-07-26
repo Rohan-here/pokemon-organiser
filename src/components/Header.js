f49c03cee4a01a55549493869f7a1bef13270828
Rohan-here <guptarohan2002@gmail.com>
2021-07-26 23:10:07 +0530

    initial setup

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className='bg-red-700 w-screen h-16 flex justify-center items-center'>
+            <text className='font-Jura font-bold text-4xl text-white text-center'>
+                Pokémon Organiser
+            </text>
+        </div>
+    )
+}
+
+export default Header;
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  theme: {
+    // Some useful comment
+    extend: {
+      fontFamily: {
+        'Jura': ['Jura', 'sans-serif']
+      },
+    },
+  },
+  variants: {
+    // Some useful comment
+  },
+  plugins: [
+    // Some useful comment
+  ]
+}
