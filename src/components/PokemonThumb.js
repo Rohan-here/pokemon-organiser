5ee2d815446cb6008d457ff55cd2ed60f7c3748e
Rohan-here <guptarohan2002@gmail.com>
2021-08-02 18:35:42 +0530

    added hover to elements

--- src/components/PokemonThumb.js
+++ src/components/PokemonThumb.js
@@ -17,7 +17,7 @@ const PokemonThumb = ({ pokemon }) => {
 
     return (
         <div className='flex items-center justify-center'>
-            <div className='border-4 border-black rounded-lg w-thumbSize my-1 flex flex-row mini:w-mobileSize'>
+            <div className='border-4 border-black rounded-lg w-thumbSize my-1 flex flex-row mini:w-mobileSize hover:border-gray-200'>
 
                 <img src={Pokemon.front_img} alt={defaultImg} className='px-3 mini:h-20 ' />
                 <div className='flex flex-col font-Jura py-1 px-2 mini:text-sm w-60 ml-3' >
@@ -26,7 +26,7 @@ const PokemonThumb = ({ pokemon }) => {
                     <span> Height : {heightConvert(Pokemon.height)}</span>
                     <span> Weight : {weightConvert(Pokemon.weight)} </span>
                 </div>
-                <div className='flex flex-col items-center justify-center ml-3'>
+                <div className='flex flex-col items-center justify-center m-3'>
                     {Pokemon.types.map(type => (
                         <span className={`font-mono text-white bg-${type} my-1 rounded-lg text-center p-1`}>{firstUpper(type)}</span>
                     ))}
